10a1016748565c405bcd4cd7844767a61b897d61
Youssef Mahmoud <ymahmoud1b@gmail.com>
2020-03-27 20:05:04 +0800

    add profile placeholder

--- frontend/src/components/scripts/Profile.js
+++ frontend/src/components/scripts/Profile.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Profile extends React.Component {
   render() {
-    return <h1>Profile</h1>;
+    return <h1 className="bodyText">Please login to see profile</h1>;
   }
 }
 
