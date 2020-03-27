a52386fda8bf7b7a109d496fffaeb8f8af7b5e56
Youssef Mahmoud <ymahmoud1b@gmail.com>
2020-03-27 11:38:40 +0800

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+class User {
+  constructor(userId, name, email, ImageList) {
+    this.userId = userId;
+    this.name = name;
+    this.email = email;
+    this.ImageList = ImageList;
+  }
+
+  getId() {
+    return this.userId;
+  }
+
+  rateImage() {
+    //TODO Implement rateImage
+  }
+
+  uploadImage() {
+    //TODO Implement uploadImage
+  }
+
+  login() {
+    //TODO Implement login
+  }
+
+  signUp() {
+    //TODO Implement signUp
+  }
+}
