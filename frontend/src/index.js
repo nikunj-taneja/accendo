04748f31e6549f75238939f5a4625381e06240ad
Youssef Mahmoud <ymahmoud1b@gmail.com>
2020-03-27 12:40:11 +0800

    add react-boostrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./components/scripts/App";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 ReactDOM.render(<App></App>, document.querySelector("#root"));
