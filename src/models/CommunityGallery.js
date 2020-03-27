72068d82a774336a7cbf09b8f88491978326e142
Youssef Mahmoud <ymahmoud1b@gmail.com>
2020-03-27 11:48:01 +0800

    add CommunityGallery model

---
+++ src/models/CommunityGallery.js
@@ -0,0 +1,13 @@
+class CommunityGallery {
+  constructor(imageList) {
+    this.imageList = imageList;
+  }
+
+  sort() {
+    //TODO implement CommunityGallery.sort
+  }
+
+  loadImages() {
+    //TODO implement CommunityGallery.loadImages
+  }
+}
