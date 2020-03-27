f6e46bb23e34ebfd51bfdc9f5cc85d0dc22f9110
Youssef Mahmoud <ymahmoud1b@gmail.com>
2020-03-27 20:22:33 +0800

    implement signup skeleton

--- frontend/src/components/scripts/SignUp.js
+++ frontend/src/components/scripts/SignUp.js
@@ -1,8 +1,42 @@
 import React from "react";
 
+import { Form, Button, Container } from "react-bootstrap";
+
+import "../styles/SignUp.css";
+
 class SignUp extends React.Component {
   render() {
-    return <h1>Sign Up</h1>;
+    return (
+      <Form>
+        <Form.Group>
+          <Form.Label className="bodyText">Name</Form.Label>
+          <Form.Control size="lg" type="text" className="bodyInput" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label className="bodyText">Email</Form.Label>
+          <Form.Control size="lg" type="email" className="bodyInput" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label className="bodyText">Password</Form.Label>
+          <Form.Control
+            size="lg"
+            type="password"
+            className="bodyInput"
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label className="bodyText">Confirm Password</Form.Label>
+          <Form.Control
+            size="lg"
+            type="password"
+            className="bodyInput"
+          ></Form.Control>
+        </Form.Group>
+        <div className="text-right">
+          <Button size="lg">Submit</Button>
+        </div>
+      </Form>
+    );
   }
 }
 
