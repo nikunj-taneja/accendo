f214838650455b7c915dae9f70caaa5aa0ee4873
Youssef Mahmoud <ymahmoud1b@gmail.com>
2020-03-27 11:34:29 +0800

    init file structure

---
+++ src/components/scripts/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../styles/App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Accendo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
