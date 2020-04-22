a3cf60ec152453fc0bc51466bd364737180e217f
Youssef Mahmoud <ymahmoud1b@gmail.com>
2020-04-22 18:09:32 +0800

    add buttons and styling

--- frontend/src/components/scripts/Profile.js
+++ frontend/src/components/scripts/Profile.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import AuthContext from "./AuthContext";
 
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import Dropzone from "react-dropzone";
 import "../styles/Profile.css";
 
@@ -11,8 +11,9 @@ class Profile extends React.Component {
   render() {
     return (
       <div className="bodyText">
-        <h5 className="pb-5 pt-5">Username: {this.context.username}</h5>
+        <h5 className="pb-5 pt-3">Hi, {this.context.username}!</h5>
 
+        <h4 className="pb-4">Add Image: </h4>
         <Dropzone
           onDrop={(acceptedFiles) => console.log(acceptedFiles)}
           accept="image/*"
@@ -24,6 +25,15 @@ class Profile extends React.Component {
             </div>
           )}
         </Dropzone>
+
+        <Container className="text-center pt-2">
+          <Button size="lg" block className="mt-3">
+            Supersize
+          </Button>
+          <Button size="lg" block className="mt-3">
+            Stylize
+          </Button>
+        </Container>
       </div>
     );
   }
