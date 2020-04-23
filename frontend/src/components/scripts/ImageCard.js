8de1857f2beb5de45d0e3b9fece6ed323a4c2fd9
Youssef Mahmoud <ymahmoud1b@gmail.com>
2020-04-24 00:13:46 +0800

    functional like

--- frontend/src/components/scripts/ImageCard.js
+++ frontend/src/components/scripts/ImageCard.js
@@ -3,14 +3,40 @@ import { Image, Container, Col } from "react-bootstrap";
 import "../styles/ImageCard.css";
 import { FaHeart } from "react-icons/fa";
 import AuthContext from "./AuthContext";
+import axios from "axios";
 
 class ImageCard extends React.Component {
+  state = { liked: false };
+
   static contextType = AuthContext;
-  static img = this.props.img;
+
+  componentWillMount() {
+    this.img = this.props.img;
+
+    this.state.liked = this.img.likes.includes(this.context.username);
+  }
 
   like = () => {
-    this.img.like_count++;
-    this.img.likes.push(this.context.username);
+    if (!this.state.liked) {
+      this.img.like_count++;
+      this.img.likes.push(this.context.username);
+      this.setState({ liked: true });
+    } else {
+      this.img.like_count--;
+      var index = this.img.likes.indexOf(this.context.username);
+      this.img.likes.splice(index, 1);
+      this.setState({ liked: false });
+    }
+
+    var data = new FormData();
+    data.set("username", this.context.username);
+    data.set("file_id", this.img.file_id);
+    axios({
+      method: "post",
+      url: "/like",
+      data: data,
+      headers: { "Content-Type": "multipart/form-data" },
+    });
   };
 
   render() {
@@ -24,13 +50,13 @@ class ImageCard extends React.Component {
           </h5>
           {this.context.isAuth ? (
             <FaHeart
-              style={{ position: "absolute", right: 20, bottom: 10 }}
+              style={{
+                position: "absolute",
+                right: 20,
+                bottom: 10,
+                color: this.state.liked ? "#28b161" : "#243b55",
+              }}
               size="1.2em"
-              color={
-                this.img.likes.includes(this.context.username)
-                  ? "#28b161"
-                  : "#black"
-              }
               onClick={this.like}
             />
           ) : (
